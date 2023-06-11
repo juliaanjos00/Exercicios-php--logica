3359a51ac79104a3b2e86af7beafdf0d95d28e38
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-06-10 21:01:53 -0300

    arquivo index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>       
+
+ <body> 
+     <ul> 
+         <li><a href= "./Exercicio-1/" target= "_blank"> Exercicio 1 </a> </li>
+     </ul>
+
+
+ </body>
+</html>
