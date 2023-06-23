6c2fdd096932583c29176bffbc317611ee1935ce
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-06-22 22:58:54 -0300

    adicionado o exercicio 2 no index

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
  <body> 
      <ul> 
          <li><a href= "./Exercicio-1/" target= "_blank"> Exercicio 1 </a> </li>
+         <li><a href= "./Exercicio-2/" target= "_blank"> Exercicio 2 </a> </li>
      </ul>
 
 
