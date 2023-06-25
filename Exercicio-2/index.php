56fd8b44a67064d1a3234df18cb4d19dfd0a5e98
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-06-24 21:14:27 -0300

    corrigi a identaçao,retirei o strong,mudei o id de titulo para h1,retirei todos os br,retirei um id text,fechei a div do div class,retirei o h3 do botao de enviar

--- Exercicio-2/index.php
+++ Exercicio-2/index.php
@@ -27,21 +27,6 @@
               </div>
                <?php
 
-                  if(isset($_POST['value']) ) {
-                  $number = $_POST['value'];
-
-                    if ($number%10 == 0 && $number%5 == 0 && $number%2 == 0) echo "<div class='results'>O número  $number, é divisível por 10, por 5 e por 2!</div>";
-                    elseif ($number%5 == 0 && $number%2 == 0)  echo "<div class='results'>O número  $number, é divisível por 5 e por 2.</div>";
-                    elseif ($number%5 == 0 && $number%10 == 0)  echo "<div class='results'>O número  $number, é divisível por 5 e por 10.</div>";
-                    elseif ($number%10 == 0 && $number%2 == 0)  echo "<div class='results'>O número  $number, é divisível por 10 e por 2.</div>";
-                    elseif ($number%10 == 0)  echo "<div class='results'>O número escolhido: $number, é divisível por 10.</div>";
-                    elseif ($number%5 == 0)  echo "<div class='results'>O número  $number, é divisível por 5.</div>";
-                    elseif ($number%2 == 0)  echo "<div class='results'>O número  $number, é divisível por 2.</div>";                     
-                    else echo "<div class='results'>O número  $number,não é divisível por 10, nem por 5 e nem por 2.</div>";
-                       }
-
-                ?>
-
-         </fieldset>
-      </body>
+    </fieldset>
+ </body>
 </html>       
