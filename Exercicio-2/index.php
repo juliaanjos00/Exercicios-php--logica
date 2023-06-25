ecc07da24b444bf4dfed8da61fe4873df017d667
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-06-24 21:14:27 -0300

    corrigi a identaçao,retirei o strong,mudei o id de titulo para h1,retirei todos os br,retirei um id text,fechei a div do div class,retirei o h3 do botao de enviar

--- Exercicio-2/index.php
+++ Exercicio-2/index.php
@@ -1,33 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+ <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/Exercicio-2/style.css">
     <title>Exercicio-2</title>
-</head>
+ </head>
 
- <body>
+       <body>
 
-   <fieldset id= "box">
-            <div> <strong><h1 id="titulo"> Exercício 2 </strong><h1>  </div> </br>
-              <p>
-              <id="text"><h2 id="text">
-              Entrar com um número e o algoritimo informará se ele é divisível por 10, por 5, por 2 ou se não é divisível por nenhum destes.
-             </h2>
-             </p>
-             
-          <div>
-            <form id="formulario" action="/Exercicio-2/index.php" method="post">
-               <div class="input-field">
-               <h3> <p><label for="number">Número:</label></p></h3>
-               <input type="number" name="value" value="<?= $num ?>" id="valor1" placeholder="Escolha um valor..."/>
-               <div class="underline"></div>
-               <br> <h3> <input type="submit" name="enviar" value="Enviar"/> </br>
-            </form>
-         </div>
+         <fieldset id= "box">
+            <div> <h1> Exercício 2</h1> </div>
+                <h2 id="text">
+                 Entrar com um número e o algoritimo informará se ele é divisível por 10, por 5, por 2 ou se não é divisível por nenhum destes               
+                </h2>
+               
+             <div>
+                <form id="formulario" action="/Exercicio-2/index.php" method="post">
+                   <div class="input-field"></div>
+                   <h3> <p><label for="number">Número:</label></p></h3>
+                   <input type="number" name="value" value="<?= $num ?>" id="valor1" placeholder="Escolha um valor..."/>
+                   <div class="underline"></div>
+                   <input type="submit" name="enviar" value="Enviar"/> 
+                </form>
+              </div>
 
-    </fieldset>
- </body>
+         </fieldset>
+      </body>
 </html>       
