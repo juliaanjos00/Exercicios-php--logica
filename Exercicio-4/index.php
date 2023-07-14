58ceeea7fb62026d5ec920db7eadcb5a6ce19507
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-07-14 15:31:12 -0300

    corrigi a indentaçao no index e retirei espaçamento desnecessario

--- Exercicio-4/index.php
+++ Exercicio-4/index.php
@@ -7,22 +7,25 @@
     <link rel="stylesheet" type="text/css" href="/Exercicio-4/style.css">
     <title>Exercicio-4</title>
  </head>
-        <body>
+       <body>
              <div class="form">
-                <h1 class="title"> Exercício 4<h1> 
-                    <h3 id="text">Entrar com 3 números e imprimi-los em
+             <h1 class="title"> Exercício 4<h1> 
+             <h3 id="text">Entrar com 3 números e imprimi-los em
                         ordem decrescente (suponha números
                         diferentes)
-                    </h3>
+             </h3>
 
-                    <form id="form" action="/Exercicio-4/index.php" method="post">
-                     <div class="input-field"></div>
-                     <label for="number" class="sub">Números:</label>
-                     <input type="number" class="input" name="value 1" id="valor 1" placeholder="Escolha um valor..."/>
-                     <input type="number" class="input" name="value 2" id="valor 2" placeholder="Escolha um valor..."/>
-                     <input type="number" class="input" name="value 3" id="valor 3" placeholder="Escolha um valor..."/>
-                     <div class="underline"></div>
-                     <input type="submit" class="button" name="enviar" value="Enviar"/> 
-                   </form>
+             <form id="form" action="/Exercicio-4/index.php" method="post">
+                <div class="input-field"></div>
+                <label for="number" class="sub">Números:</label>
+                <input type="number" class="input" name="value 1" id="valor 1" placeholder="Escolha um valor..."/>
+                <input type="number" class="input" name="value 2" id="valor 2" placeholder="Escolha um valor..."/>
+                <input type="number" class="input" name="value 3" id="valor 3" placeholder="Escolha um valor..."/>
+                <div class="underline"></div>
+                <input type="submit" class="button" name="enviar" value="Enviar"/> 
+             </form>
+      </body>         
+
+</html>                    
               
                 
