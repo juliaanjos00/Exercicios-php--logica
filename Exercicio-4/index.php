363e7efc0c7d1af6c287f486a1db8320083b36ac
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-07-14 00:53:12 -0300

    envio do frontend do exercicio 4

---
+++ Exercicio-4/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/Exercicio-4/style.css">
+    <title>Exercicio-4</title>
+ </head>
+        <body>
+             <div class="form">
+                <h1 class="title"> Exercício 4<h1> 
+                    <h3 id="text">Entrar com 3 números e imprimi-los em
+                        ordem decrescente (suponha números
+                        diferentes)
+                    </h3>
+
+                    <form id="form" action="/Exercicio-4/index.php" method="post">
+                     <div class="input-field"></div>
+                     <label for="number" class="sub">Números:</label>
+                     <input type="number" class="input" name="value 1" id="valor 1" placeholder="Escolha um valor..."/>
+                     <input type="number" class="input" name="value 2" id="valor 2" placeholder="Escolha um valor..."/>
+                     <input type="number" class="input" name="value 3" id="valor 3" placeholder="Escolha um valor..."/>
+                     <div class="underline"></div>
+                     <input type="submit" class="button" name="enviar" value="Enviar"/> 
+                   </form>
+              
+                
