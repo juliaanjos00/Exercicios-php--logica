1df7a87cbae52e1795689d195af3e271aad09f1c
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-07-28 23:28:57 -0300

    corrigi a indentaçao

--- Exercicio-5/index.php
+++ Exercicio-5/index.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/Exercicio-5/style.css">
-    <title>Exercicio-5</title>
- </head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" type="text/css" href="/Exercicio-5/style.css">
+      <title>Exercicio-5</title>
+  </head>
        <body>
              <div class="form">
              <div class="title"> <h1> Exercício 5<h1> </div>
-             <h3 id="text">Ler 3 números, os possíveis lados de um
-                 triângulo, e imprimir a classificação de
-                 acordo com tamanho dos lados
-             </h3>
+                <h3 id="text">Ler 3 números, os possíveis lados de um
+                  triângulo, e imprimir a classificação de
+                  acordo com tamanho dos lados
+                </h3>
 
-             <form id="form" action="/Exercicio-5/index.php" method="post">
-                <div class="input-field"></div>
-                <label for="number">Escolha os números:</label>
-                <input type="number" class="input" name="value1" id="value1" placeholder="Escolha um valor..."/>
-                <input type="number" class="input" name="value2" id="value2" placeholder="Escolha um valor..."/>
-                <input type="number" class="input" name="value3" id="value3" placeholder="Escolha um valor..."/>
-                <div class="underline"></div>
-                <input type="submit" class="button" name="enviar" value="Enviar"/> 
-             </form>
+                <form id="form" action="/Exercicio-5/index.php" method="post">
+                  <div class="input-field"></div>
+                  <label for="number">Escolha os números:</label>
+                  <input type="number" class="input" name="value1" id="value1" placeholder="Escolha um valor..."/>
+                  <input type="number" class="input" name="value2" id="value2" placeholder="Escolha um valor..."/>
+                  <input type="number" class="input" name="value3" id="value3" placeholder="Escolha um valor..."/>
+                  <div class="underline"></div>
+                  <input type="submit" class="button" name="enviar" value="Enviar"/> 
+              </form>
+
+       </body>  
+</html>
