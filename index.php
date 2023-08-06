466fe35aace6241e60633def359c943e9ac8cc58
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-08-05 22:53:43 -0300

    adicionado exercicio 6 no index

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
          <li><a href= "./Exercicio-1/" target= "_blank"> Exercicio 1 </a> </li>
          <li><a href= "./Exercicio-2/" target= "_blank"> Exercicio 2 </a> </li>
          <li><a href= "./Exercicio-3/" target= "_blank"> Exercicio 3 </a> </li>
-         <li><a href= "./Exercicio-5/" target= "_blank"> Exercicio 5 </a> </li>
+         <li><a href= "./Exercicio-4/" target= "_blank"> Exercicio 4 </a> </li>
+         <li><a href= "./Exercicio-6/" target= "_blank"> Exercicio 6 </a> </li>
      </ul>
 
 
