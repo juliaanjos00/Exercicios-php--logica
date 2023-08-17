3f272ce146f986522291c25a01d673d22be19bdb
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-08-16 22:20:32 -0300

    corrigi a indentaçao,coloquei o break faltando referente ao mês 3  e adicionei o defeault

--- Exercicio-6/index.php
+++ Exercicio-6/index.php
@@ -25,55 +25,54 @@
                 </form>
 
                 <?php
+                
+                if (isset( $_POST['Mes' ])){
 
-if (isset( $_POST['Mes' ])){
+                      $Mes= $_POST['Mes'];
 
-    $Mes= $_POST['Mes'];
-
-
-    switch($Mes){
-      case '1':
-       echo "Janeiro";
-      break;
-      case '2':
-       echo "Fevereiro";
-      break;
-      case '3':
-       echo  "Março";
-       case '4':
-        echo "Abril";
-       break;
-       case '5':
-        echo "Maio";
-       break;
-       case '6':
-        echo "Junho";
-       break;
-       case '7':
-        echo "Julho";
-       break;
-       case '8':
-        echo "Agosto";
-       break;
-       case '9':
-        echo "Setembro";
-       break;  case '10':
-        echo "Outubro";
-       break;
-       case '11':
-        echo "Novembro";
-       break;
-       case '12':
-        echo "Dezembro";
-       break;
-     }
-     } 
-
-
-
-
+                      switch($Mes){
+                        case '1':
+                        echo "Janeiro";
+                        break;
+                        case '2':
+                          echo "Fevereiro";
+                          break;
+                        case '3':
+                          echo  "Março";
+                          break;
+                        case '4':
+                          echo "Abril";
+                          break;
+                        case '5':
+                          echo "Maio";
+                           break;
+                        case '6':
+                          echo "Junho";
+                          break;
+                        case '7':
+                          echo "Julho";
+                          break;
+                        case '8':
+                          echo "Agosto";
+                        break;
+                        case '9':
+                          echo "Setembro";
+                        break;  
+                        case '10':
+                          echo "Outubro";
+                           break;
+                        case '11':
+                          echo "Novembro";
+                          break;
+                        case '12':
+                          echo "Dezembro";
+                          break;
+                        default:
+                          echo "Opção inválida.";
+                      }
+                      } 
                
-             ?>
+               ?>
                 
             </div>
        </body> 
