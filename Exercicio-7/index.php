ca7a7a08692d6ff7ffec90662a3696c0e4c05bfb
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-08-25 23:44:04 -0300

    fazendo estilizaçao

--- Exercicio-7/index.php
+++ Exercicio-7/index.php
@@ -7,33 +7,34 @@
     <link rel="stylesheet" type="text/css" href="/Exercicio-7/style.css">
     <title>Exercicio-7</title>
  </head>
-       <body>
-       <div class="slider">
+  <body>
+        <div class="slider">
+        <div class="titles">LIVROS DISPONÍVEIS</div>
        <div class="slides">
-         <input type="radio" name="radio-btn" id="radio1">
-         <input type="radio" name="radio-btn" id="radio2">
-         <input type="radio" name="radio-btn" id="radio3">
-         <input type="radio" name="radio-btn" id="radio4">
-         <input type="radio" name="radio-btn" id="radio5">
-         <input type="radio" name="radio-btn" id="radio6">
-         <input type="radio" name="radio-btn" id="radio7">
-         <input type="radio" name="radio-btn" id="radio8">
-         <input type="radio" name="radio-btn" id="radio9">
-         <input type="radio" name="radio-btn" id="radio10">
+         <input type="radio" class="radio" name="radio-btn" id="radio1">
+         <input type="radio" class="radio" name="radio-btn" id="radio2">
+         <input type="radio" class="radio" name="radio-btn" id="radio3">
+         <input type="radio" class="radio" name="radio-btn" id="radio4">
+         <input type="radio" class="radio" name="radio-btn" id="radio5">
+         <input type="radio" class="radio" name="radio-btn" id="radio6">
+         <input type="radio" class="radio" name="radio-btn" id="radio7">
+         <input type="radio" class="radio" name="radio-btn" id="radio8">
+         <input type="radio" class="radio" name="radio-btn" id="radio9">
+         <input type="radio" class="radio" name="radio-btn" id="radio10">
 
-                  <div class="books">
-                    <img src="./img1"/>
+                  <div class="slide first">
+                    <img src="./img1">
                    </div>
 
-                  <div class="books">
+                  <div class="slide">
                     <img src="./img2"/>
                    </div>
 
-                  <div class="books">
+                  <div class="slide">
                     <img src="./img3"/>
                    </div>
 
-                  <div class="books">
+                  <div class="slide">
                     <img src="./img4" />
                    </div>
 
@@ -41,43 +42,43 @@
                     <img src="./img5"/>
                    </div>
 
-                  <div class="books">
+                  <div class="slide">
                     <img src="./img6"/>
                    </div>
 
-                  <div class="books">
+                  <div class="slide">
                     <img src="./img7"/>
                    </div>
                      
-                  <div class="books">
+                  <div class="slide">
                     <img src="./img8"/>
                    </div>
 
-                  <div class="books">
+                  <div class="slide">
                     <img src="./img9" />
                    </div>
 
-                  <div class="books">
+                  <div class="slide">
                     <img src="./img10" />
                    </div>
 
-                   <div class="navegation-bar">
-                     <label for="radio1">Botao 1</label>
-                     <label for="radio2">Botao 2</label>
-                     <label for="radio3">Botao 3</label>
-                     <label for="radio4">Botao 4</label>
-                     <label for="radio5">Botao 5</label>
-                     <label for="radio6">Botao 6</label>
-                     <label for="radio7">Botao 7</label>
-                     <label for="radio8">Botao 8</label>
-                     <label for="radio9">Botao 9</label>
-                     <label for="radio10">Botao 10</label>
+                   <div class="navigation-bar">
+                     <label class= "manual-btn" for="radio1"></label>
+                     <label class= "manual-btn" for="radio2"></label>
+                     <label class= "manual-btn" for="radio3"></label>
+                     <label class= "manual-btn" for="radio4"></label>
+                     <label class= "manual-btn" for="radio6"></label>
+                     <label class= "manual-btn" for="radio7"></label>
+                     <label class= "manual-btn" for="radio8"></label>
+                     <label class= "manual-btn" for="radio9"></label>
+                     <label class= "manual-btn" for="radio10"></label>
                   </div>
                   
                 </div>  
                </div>
 
-            <div class="form">
+              
+               <div class="form">
                 <h1 class="title"> Exercício 7</h1> 
                 <div class="text">A biblioteca de uma universidade deseja fazer
                              um algoritmo que leia o nome do livro que
@@ -85,20 +86,19 @@
                              ou aluno) e possa imprimir um recibo
                              conforme mostrado a seguir. Considerar que
                              o professor tem 10 dias para devolver o livro
-                             o aluno somente 3 dias
-
-                 <form id="form" action="/Exercicio-7/index.php" method="post">
+                             o aluno somente 3 dias. </div>
+                <form id="form" action="/Exercicio-7/index.php" method="post">
                     <div class="input-field"></div>
-                    <label for="number" class="sub">Professor(a):</label>
-                    <input type="number" class="input" name="Professor" id="value" placeholder="..."/>
-                    <label for="number" class="sub">Aluno(a):</label>
-                    <input type="number" class="input" name="Aluno" id="value" placeholder="..."/>
-                    <label for="number" class="sub">Livro:</label>
-                    <input type="number" class="input" name="Livro" id="value" placeholder="..."/>
+                    <label for="text" class="sub">Professor(a):</label>
+                    <input type="text" class="placeholder" name="Professor" id="value" placeholder="..."/>
+                    <label for="text" class="sub">Aluno(a):</label>
+                    <input type="text" class="placeholder" name="Aluno" id="value" placeholder="..."/>
+                    <label for="text" class="sub">Livro:</label>
+                    <input type="text" class="placeholder" name="Livro" id="value" placeholder="..."/>
                     <div class="underline"></div>
                     <input type="submit" class="button" name="enviar" value="Enviar"/> 
-                 </form>
+                </form>
                 </div>
+              
 
-                
-               
+        
