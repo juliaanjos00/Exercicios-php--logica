c3f16be07fdf093b833704bf91132e6f0051c448
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-09-30 19:40:15 -0300

    frontend do exercicio 10

---
+++ Exercicio-10/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" type="text/css" href="/Exercicio-10/style.css">
+      <title>Exercicio-10</title>
+  </head>
+       <body>
+             <div class="form">
+             <div class="title">  Exercício 10 </div>
+                <h3 id="text">Entrar com 20 números e imprimir a soma
+                             dos positivos e o total de números negativos
+                </h3>
+
+                <form id="form" action="/Exercicio-10/index.php" method="post">
+                  <div class="input-field"></div>
+                  <label for="number">Escolha 20 números:</label>
+                  <input type="number" class="input" name="number" id="number" placeholder="Escolha um valor..."/>
+                  <div class="underline"></div>
+                  <input type="submit" class="button" name="enviar" value="Enviar"/> 
+                </form>
+
+        </body>
+</html>            
