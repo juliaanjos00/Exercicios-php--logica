8f6b9db2fff4a9dd120ed38f65f82788ea72a4c1
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-10-06 23:59:00 -0300

    adicionado exercicio 10 no indice

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
          <li><a href= "./Exercicio-5/" target= "_blank"> Exercicio 5 </a> </li>
          <li><a href= "./Exercicio-6/" target= "_blank"> Exercicio 6 </a> </li>
          <li><a href= "./Exercicio-8/" target= "_blank"> Exercicio 8 </a> </li>
+         <li><a href= "./Exercicio-10/" target= "_blank"> Exercicio 10 </a> </li>
      </ul>
 
 
