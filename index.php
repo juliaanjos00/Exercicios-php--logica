4ec97567fddb5bb012e87239091b13d6f7af4cd4
juliaanjos0 <anjosjulianjos09@gmail.com>
2023-12-10 12:41:48 -0300

    coloquei ate o exercicio 20 no indice

--- index.php
+++ index.php
@@ -12,7 +12,18 @@
          <li><a href= "./Exercicio-5/" target= "_blank"> Exercicio 5 </a> </li>
          <li><a href= "./Exercicio-6/" target= "_blank"> Exercicio 6 </a> </li>
          <li><a href= "./Exercicio-8/" target= "_blank"> Exercicio 8 </a> </li>
+         <li><a href= "./Exercicio-9/" target= "_blank"> Exercicio 9 </a> </li>
          <li><a href= "./Exercicio-10/" target= "_blank"> Exercicio 10 </a> </li>
+         <li><a href= "./Exercicio-11/" target= "_blank"> Exercicio 11 </a> </li>
+         <li><a href= "./Exercicio-12/" target= "_blank"> Exercicio 12 </a> </li>
+         <li><a href= "./Exercicio-13/" target= "_blank"> Exercicio 13 </a> </li>
+         <li><a href= "./Exercicio-14/" target= "_blank"> Exercicio 14 </a> </li>
+         <li><a href= "./Exercicio-15/" target= "_blank"> Exercicio 15 </a> </li>
+         <li><a href= "./Exercicio-16/" target= "_blank"> Exercicio 16 </a> </li>
+         <li><a href= "./Exercicio-17/" target= "_blank"> Exercicio 17 </a> </li>
+         <li><a href= "./Exercicio-18/" target= "_blank"> Exercicio 18 </a> </li>
+         <li><a href= "./Exercicio-19/" target= "_blank"> Exercicio 19 </a> </li>
+         <li><a href= "./Exercicio-20/" target= "_blank"> Exercicio 20 </a> </li>
      </ul>
 
 
